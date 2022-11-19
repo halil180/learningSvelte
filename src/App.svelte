b1926c3c929d072216d948dd139b046742941ef6
halil180 <halilkom180@.com>
2022-11-19 20:26:12 +0100

    data binding

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import Counter from './lib/Counter.svelte'
+
+  let color = "black"
+  const handleClick = () => {
+    color = "orange"
+  }
+  // const handleInput = (e) => {
+  //   color = e.target.value
+  // }
+</script>
+
+<main>
+  <h1 style="color: {color}">{color}</h1>
+
+<button on:click={handleClick}>update color</button>
+
+<!-- <input type="text" on:input={handleInput}  value={color}> -->
+<input type="text" bind:value={color}> <!--shorthand for the input field above -->
+</main>
+
+<style >
+
+</style>
