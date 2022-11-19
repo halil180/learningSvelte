ae07af7875471e0591a1a659331ceacf252ac06e
halil180 <halilkom180@.com>
2022-11-19 20:48:58 +0100

    reactive value

--- src/App.svelte
+++ src/App.svelte
@@ -2,22 +2,35 @@
   import svelteLogo from './assets/svelte.svg'
   import Counter from './lib/Counter.svelte'
 
+$:fullname= `${firstName} ${lastName}` /// reactive value
+
   let color = "black"
   const handleClick = () => {
     color = "orange"
   }
+
+  $:{
+    console.log(firstName)
+    console.log(color)
+  }
+
   // const handleInput = (e) => {
   //   color = e.target.value
   // }
+  let firstName = "jimi"
+  let lastName = "hendrix"
 </script>
 
 <main>
   <h1 style="color: {color}">{color}</h1>
-
+  <p>{fullname}</p>
 <button on:click={handleClick}>update color</button>
 
 <!-- <input type="text" on:input={handleInput}  value={color}> -->
+
 <input type="text" bind:value={color}> <!--shorthand for the input field above -->
+<input type="text" bind:value={firstName}>
+<input type="text" bind:value={lastName}>
 </main>
 
 <style >
